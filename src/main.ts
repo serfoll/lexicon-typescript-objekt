fc475f4f485111ee6efc903aee011521b86ee5b2
Ser Foll <sergiofollyvi@gmail.com>
2026-01-12 14:16:32 +0100

    Exercise 1 & 2: complete

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,37 @@
-console.log("Hello, TS objects");
+// Steg 1: Object
+const fiende: {
+  namn: string;
+  skada: number;
+  hp: number;
+  ras: string;
+  present: () => void;
+  anfall: () => void;
+} = {
+  namn: "Great Wolf",
+  skada: 20,
+  hp: 50,
+  ras: "wolf",
+  present() {
+    console.log(
+      `Namn: ${this.namn}, Ras: ${this.ras}, HP: ${this.hp}, Skada: ${this.skada}`,
+    );
+  },
+  anfall() {
+    console.log(`${this.namn} har gjort ${this.skada} i skada!`);
+  },
+};
+
+fiende.present();
+const fiendeNamn: string = fiende["namn"];
+const fiendeSkada: number = fiende.skada;
+console.log(`Namn: ${fiendeNamn}, skada: ${fiendeSkada}`);
+
+fiende.anfall();
+
+// Steg 2: Math & Date
+const randNum: number = Math.floor(Math.random() * 100) + 1; // generera ett värde mellan 1 och 100
+const nu: Date = new Date();
+console.log(`slump tal: ${randNum}, nuvarande år: ${nu.getFullYear()}`);
+
+// Steg 3: Typkonvertering
+// Steg 4: Slutuppgift
